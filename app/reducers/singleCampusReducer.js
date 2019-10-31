9cb2c8fe536737b1d468661ee9c1cc6f070fd112
David-Sanvicente <sanvicented1@gmail.com>
2019-10-31 13:45:08 -0400

    tier 3 complete

--- app/reducers/singleCampusReducer.js
+++ app/reducers/singleCampusReducer.js
@@ -1,34 +1,3 @@
-// import axios from "axios";
-
-// // action types
-// const GOT_SINGLE_CAMPUS = "GOT_SINGLE_CAMPUS";
-
-// // action creators
-// const gotSingleCampus = singleCampus => {
-//   return {
-//     type: GOT_SINGLE_CAMPUS,
-//     singleCampus
-//   };
-// };
-
-// // thunks
-// export const getSingleCampus = id => async dispatch => {
-//   const { data } = await axios.get(`/api/campuses/${id}`);
-//   dispatch(gotSingleCampus(data));
-// };
-
-// // reducer
-// const singleCampusReducer = (state = [], action) => {
-//   switch (action.type) {
-//     case GOT_SINGLE_CAMPUS:
-//       return action.singleCampus;
-//     default:
-//       return state;
-//   }
-// };
-
-// export default singleCampusReducer;
-
 import axios from "axios";
 
 // action types
